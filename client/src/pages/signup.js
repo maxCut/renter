51b6097c082bbe3e6b76d0c4ee94f50d0e87de15
maxCut <maxCut@github.com>
2019-11-29 02:46:34 -0500

    added documentation and fixed up login/signup

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -5,7 +5,15 @@ class SignUp extends Component {
     {
         return(
             <div className = "SignUpPage">
-               feature coming soon 
+                <div class="container">
+                    <label for="uname"><b>Enter Desired Username</b></label>
+                    <input type="text" placeholder="Username" id ="username"></input>
+                    <label for="uname"><b>Enter Desired Password</b></label>
+                    <input type="password" placeholder="Password" id ="password"></input>
+                    <button onclick = "firebaseSignup()" id="login-btn">
+                        Signup
+                    </button>
+                </div>
             </div>
         )
 
